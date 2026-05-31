63f6ceeea5683dff31c5489fd8c4e91b4c321a0d
Tanuj <Tanuj5480@gmail.com>
2026-05-31 14:36:37 -0400

    adding bias variable

--- attention_is_all_you_need/self_attention.py
+++ attention_is_all_you_need/self_attention.py
@@ -11,14 +11,14 @@ import torch.nn.functional as F
 torch.manual_seed(123)
 class SelfAttention(nn.Module):
 
-    def __init__(self, d_in, d_out):
+    def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
         self.d_in = d_in
         self.d_out = d_out
 
-        self.W_query = nn.Linear(d_in, d_out, bias=False)
-        self.W_keys = nn.Linear(d_in, d_out, bias=False)
-        self.W_values = nn.Linear(d_in, d_out, bias=False)
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_keys = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_values = nn.Linear(d_in, d_out, bias=qkv_bias)
 
     def forward(self, x):
 
