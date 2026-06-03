f9daae5f75f3ab55cf8bb12233a585738e8bd612
Tanuj <Tanuj5480@gmail.com>
2026-06-03 09:57:09 -0400

    adding utils and finetuning task model loading

--- attention_is_all_you_need/finetune_classification.py
+++ attention_is_all_you_need/finetune_classification.py
@@ -5,9 +5,11 @@ from pathlib import Path
 import pandas as pd
 import tiktoken
 import torch
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-
+from torch.utils.data import Dataset, DataLoader
+import urllib
+# from gpt_download import download_and_load_gpt2
+from gpt import GPT
+from utils.utils import load_weights_into_gpt, generate_text_simple, generate, text_to_token_ids, token_ids_to_text
 
 
 url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
@@ -210,3 +212,52 @@ for input_batch, target_batch in train_loader:
 
 print("Input batch dimensions:", input_batch.shape)
 print("Label batch dimensions", target_batch.shape)
+
+# #####################
+# Initializing a model with pretrained weights
+# #####################
+
+CHOOSE_MODEL = "gpt2-small (124M)"
+INPUT_PROMPT = "Every effort moves"
+
+BASE_CONFIG = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,        # Dropout rate
+    "qkv_bias": True         # Query-key-value bias
+}
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+assert train_dataset.max_length <= BASE_CONFIG["context_length"], (
+    f"Dataset length {train_dataset.max_length} exceeds model's context "
+    f"length {BASE_CONFIG['context_length']}. Reinitialize data sets with "
+    f"`max_length={BASE_CONFIG['context_length']}`"
+)
+
+
+# model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+# settings, params = download_and_load_gpt2(model_size=model_size, models_dir="gpt2")
+file_name = "gpt2-small-124M.pth"
+model = GPT(BASE_CONFIG)
+model.load_state_dict(torch.load(file_name, weights_only=True))
+model.eval()
+
+
+text_1 = "Every effort moves you"
+token_ids = generate_text_simple(
+    model=model,
+    idx=text_to_token_ids(text_1, tokenizer),
+    max_new_tokens=15,
+    context_size=BASE_CONFIG["context_length"]
+)
+
+print(token_ids_to_text(token_ids, tokenizer))
+
